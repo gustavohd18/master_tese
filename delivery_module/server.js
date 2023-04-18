7b93a3f055b0a9f743dadb60b5cfda5eae3475e6
gustavohd18 <gustavo.hernandez@acad.pucrs.br>
2023-04-18 00:44:18 -0300

    add listener from named entity

--- delivery_module/server.js
+++ delivery_module/server.js
@@ -9,22 +9,88 @@ const WebSocketServer = require('ws');
 // Creating a new websocket server
 const wss = new WebSocketServer.Server({ port: 8080 })
 
-const kafka = new kafka1.Kafka({
-  clientId: 'my-app',
-  brokers: ['192.168.0.46:9092'],
-})
+// const kafka = new kafka1.Kafka({
+//   clientId: 'my-app',
+//   brokers: ['192.168.0.90:9092'],
+// })
 
- const consumer = kafka.consumer({ groupId: 'test-group' })
+ //const consumer = kafka.consumer({ groupId: 'test-group' })
 
 // Creating connection using websocket
-wss.on("connection", ws => {
+wss.on("connection", async ws => {
   console.log("new client connected");
 
   // sending message to client
-  ws.send('Welcome, you are connected!');
-  ws.send(JSON.stringify([["tes",2], ["testa4",6]]));
-  //handlerKafka(ws)
+  //ws.send('Welcome, you are connected!');
+  //ws.send(JSON.stringify([["tes",2], ["testa4",6]]));
+  // Consumer options
+const consumerConfig = {
+  groupId: 'my-group',
+  brokers: ['192.168.0.90:9092'],
+  autoCommit: true,
+  autoCommitInterval: 5000,
+};
 
+// First consumer
+const consumer1 = new  kafka1.Kafka(consumerConfig).consumer({ groupId: 'consumer1' });
+await consumer1.connect();
+await consumer1.subscribe({ topic: 'teste2' });
+await consumer1.run({
+  eachBatchAutoResolve: true,
+  eachBatch: async ({
+      batch,
+      resolveOffset,
+      heartbeat,
+      commitOffsetsIfNecessary,
+      uncommittedOffsets,
+      isRunning,
+      isStale,
+      pause,
+  }) => {
+
+    const outputList = batch.messages.map(message => {
+      // Remove the parentheses and split the string by the comma
+      const [x, value] = message.value.toString().replace(/[()]/g, '').split(',');
+      
+      // Create a new object with the extracted values
+      return  [x, parseInt(value)];
+    });
+    //format object to json 
+    ws.send(JSON.stringify({"namedEntity":false, data:outputList}));
+  },
+});
+
+// Second consumer
+const consumer2 =  new kafka1.Kafka(consumerConfig).consumer({ groupId: 'consumer2' });
+await consumer2.connect();
+await consumer2.subscribe({ topic: 'teste23' });
+await consumer2.run({
+  eachBatchAutoResolve: true,
+  eachBatch: async ({
+      batch,
+      resolveOffset,
+      heartbeat,
+      commitOffsetsIfNecessary,
+      uncommittedOffsets,
+      isRunning,
+      isStale,
+      pause,
+  }) => {
+
+    const outputList = batch.messages.map(message => {
+      // Remove the parentheses and split the string by the comma
+      const [x, value] = message.value.toString().replace(/[()]/g, '').split(',');
+      
+      // Create a new object with the extracted values
+      return  [x, parseInt(value)];
+    });
+    //format object to json 
+    ws.send(JSON.stringify({"namedEntity":true, data:outputList}));
+  },
+});
+  //handlerKafkaNamed(ws)
+
+ // handlerKafka(ws)
 
   //on message from client
   ws.on("message", data => {
@@ -63,10 +129,39 @@ async function handlerKafka (ws) {
         const [x, value] = message.value.toString().replace(/[()]/g, '').split(',');
         
         // Create a new object with the extracted values
-        return  [x, parseInt(value)] ;
+        return  [x, parseInt(value)];
       });
       //format object to json 
-      ws.send(JSON.stringify(outputList));
+      ws.send(JSON.stringify({"namedEntity":false, data:outputList}));
+    },
+})
+ }
+
+ async function handlerKafkaNamed (ws) {
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'teste23', fromBeginning: true })
+  await consumer.run({
+    eachBatchAutoResolve: true,
+    eachBatch: async ({
+        batch,
+        resolveOffset,
+        heartbeat,
+        commitOffsetsIfNecessary,
+        uncommittedOffsets,
+        isRunning,
+        isStale,
+        pause,
+    }) => {
+
+      const outputList = batch.messages.map(message => {
+        // Remove the parentheses and split the string by the comma
+        const [x, value] = message.value.toString().replace(/[()]/g, '').split(',');
+        
+        // Create a new object with the extracted values
+        return  [x, parseInt(value)];
+      });
+      //format object to json 
+      ws.send(JSON.stringify({"namedEntity":true, data:outputList}));
     },
 })
  }
